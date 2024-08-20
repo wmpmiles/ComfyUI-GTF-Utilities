99807f3182c61fa24a612082f24486c7f8485462
William Miles <wmpmiles@gmail.com>
2024-08-20 13:54:04 +1000

    Increased precision when doing colorspace conversions to minimise changes from repeated conversion.

--- gtf_impl/colorspace.py
+++ gtf_impl/colorspace.py
@@ -8,26 +8,28 @@ import torch
 
 
 def srgb_gamma_to_linear(tensor: torch.Tensor):
+    double = tensor.to(torch.float64)
     a = 0.055
-    piece_lo = tensor / 12.92
-    piece_hi = torch.pow((tensor + a) / (1 + a), 2.4)
-    tensor = torch.where(tensor <= 0.04045, piece_lo, piece_hi)
-    return tensor
+    piece_lo = double / 12.92
+    piece_hi = torch.pow((double + a) / (1 + a), 2.4)
+    linear = torch.where(double <= 0.04045, piece_lo, piece_hi).to(torch.float)
+    return linear
 
 
 def srgb_linear_to_gamma(tensor: torch.Tensor):
+    double = tensor.to(torch.float64)
     a = 0.055
-    piece_lo = tensor * 12.92
-    piece_hi = (1 + a) * torch.pow(tensor, 1/2.4) - a
-    tensor = torch.where(tensor <= 0.0031308, piece_lo, piece_hi)
-    return tensor
+    piece_lo = double * 12.92
+    piece_hi = (1 + a) * torch.pow(double, 1/2.4) - a
+    gamma = torch.where(double <= 0.0031308, piece_lo, piece_hi).to(torch.float)
+    return gamma
 
 
 def linear_to_log(tensor: torch.Tensor, eps: float) -> torch.Tensor:
-    log = torch.log2(tensor + eps)
+    log = torch.log2(tensor.to(torch.float64) + eps).to(torch.float)
     return log
 
 
 def log_to_linear(tensor: torch.Tensor, eps: float) -> torch.Tensor:
-    linear = torch.exp2(tensor) - eps
+    linear = (torch.exp2(tensor.to(torch.float64)) - eps).to(torch.float)
     return linear
