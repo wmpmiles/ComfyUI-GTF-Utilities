7a2d18bc7f65e71d3bb62bed73df6180e4dd53c8
William Miles <wmpmiles@gmail.com>
2024-08-28 16:32:58 +1000

    Refactor `luminance`.

--- gtf_impl/convert.py
+++ gtf_impl/convert.py
@@ -1,7 +1,11 @@
 import torch
 
 
+LUMINANCE = torch.tensor((0.2126, 0.7152, 0.0722)).reshape(1, 3, 1, 1)
+
+
 def to_luminance(tensor: torch.Tensor) -> torch.Tensor:
-    LUMINANCE = torch.tensor((0.2126, 0.7152, 0.0722)).reshape(1, 3, 1, 1)
+    if tensor.shape[1] != 3:
+        raise ValueErorr("Can only convert a 3 channel GTF to luminance.")
     luminance = (tensor * LUMINANCE).sum(1, keepdims=True)
     return luminance
