2184c3e92ff1e15205cbf2ef1a921337d7e3e8f6
William Miles <wmpmiles@gmail.com>
2024-10-09 21:40:35 +1100

    Renamed filters to filter and some small refactors.

--- filter.py
+++ filter.py
@@ -11,20 +11,18 @@ from math import pi
 from enum import Enum
 
 
-def convolve_2d(tensor: Tensor, kernel: Tensor) -> Tensor:
-    b, c, _, _ = tensor.shape
-    kb, kc, kh, kw = kernel.shape
+def convolve_2d(gtf_tensor: Tensor, gtf_kernel: Tensor) -> Tensor:
+    b, c, _, _ = gtf_tensor.shape
+    kb, kc, kh, kw = gtf_kernel.shape
     if kh % 2 == 0 or kw % 2 == 0:
         raise ValueError("Kernels must have odd height and width.")
     if kb != 1 and kb != b:
-        raise ValueError("A kernel with batch size greater than 1 must have \
-                         the same batch size as the GTF.")
+        raise ValueError("A kernel with batch size greater than 1 must have the same batch size as the GTF.")
     if kc != 1 and kc != c:
-        raise ValueError("A kernel with more than 1 channel must have the \
-                         same number of chdannels as the GTF.")
-    kernel_expanded = kernel.expand(b, c, -1, -1)
+        raise ValueError("A kernel with more than 1 channel must have the same number of channels as the GTF.")
+    kernel_expanded = gtf_kernel.expand(b, c, -1, -1)
     ph, pw = kh // 2, kw // 2
-    padded_h = U.pad_tensor_reflect(tensor, 2, ph, ph)
+    padded_h = U.pad_tensor_reflect(gtf_tensor, 2, ph, ph)
     padded = U.pad_tensor_reflect(padded_h, 3, pw, pw)
     convolved = []
     for (b, k) in zip(padded, kernel_expanded):
@@ -33,8 +31,8 @@ def convolve_2d(tensor: Tensor, kernel: Tensor) -> Tensor:
     return concatenated
 
 
-def gradient_suppression_mask(norm: Tensor, angle: Tensor) -> Tensor:
-    padded = U.pad_tensor_reflect(U.pad_tensor_reflect(norm, 3, 1, 1), 2, 1, 1)
+def gradient_suppression_mask(gtf_r: Tensor, gtf_theta: Tensor) -> Tensor:
+    padded = U.pad_tensor_reflect(U.pad_tensor_reflect(gtf_r, 3, 1, 1), 2, 1, 1)
     views = []
     for i in range(9):
         x, y = i % 3, i // 3
@@ -45,7 +43,7 @@ def gradient_suppression_mask(norm: Tensor, angle: Tensor) -> Tensor:
         lower = views[4] >= views[i]
         upper = views[4] >= views[8-i]
         comparisons += [torch.logical_and(lower, upper)]
-    angle_index = (((angle + (9 * pi / 8)) * (4 / pi)).to(torch.int) + 3) % 4
+    angle_index = (((gtf_theta + (9 * pi / 8)) * (4 / pi)).to(torch.int) + 3) % 4
     odd = (angle_index % 2).to(torch.bool)
     l = torch.where(odd, comparisons[1], comparisons[0])
     u = torch.where(odd, comparisons[3], comparisons[2])
@@ -53,10 +51,10 @@ def gradient_suppression_mask(norm: Tensor, angle: Tensor) -> Tensor:
     return mask
 
 
-def hysteresis_threshold(weak: Tensor, strong: Tensor) -> Tensor:
-    b, c, h, w = weak.shape
-    coloring, max_unique = TF.component_coloring(weak, True)
-    strong_components = strong.to(torch.bool) * coloring
+def hysteresis_threshold(gtf_weak: Tensor, gtf_strong: Tensor) -> Tensor:
+    b, c, h, w = gtf_weak.shape
+    coloring, max_unique = TF.component_coloring(gtf_weak, True)
+    strong_components = gtf_strong.to(torch.bool) * coloring
     batches = []
     for bi in range(b):
         channels = []
